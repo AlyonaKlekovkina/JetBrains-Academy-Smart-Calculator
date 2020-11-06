82dd54ff58d4959319153f3f5a1b595dbd2589cc
Alyona Klekovkina <like.the.sunshine.17@gmail.com>
2020-11-06 19:02:24 +0300

    Stage #5: Error!

--- calculator.py
+++ calculator.py
@@ -1,9 +1,4 @@
 # write your code here
-def addition(array):
-    the_sum = 0
-    for i in array:
-        the_sum += i
-    return the_sum
 
 
 def determine_sign(array):
@@ -21,35 +16,49 @@ def determine_sign(array):
 
 
 def determine_ints(array):
-    integers = []
-    for j in range(0, len(array), 2):
-        integers.append(int(array[j]))
-    return integers
+    try:
+        integers = []
+        for j in range(0, len(array), 2):
+            if array[j].startswith("+"):
+                s = array[j].split('+')
+                integers.append(s[1])
+            elif array[j].endswith("+"):
+                return "Invalid expression"
+            else:
+                integers.append(int(array[j]))
+        return integers
+    except ValueError:
+        return "Invalid expression"
 
 
 def calculate_expression(signs, integers):
-    result_of_calculation = integers[0]
-    for i in range(len(integers) - 1):
-        if signs[i] == '+':
-            result_of_calculation += integers[i+1]
-        elif signs[i] == '-':
-            result_of_calculation -= integers[i+1]
-    return result_of_calculation
+    try:
+        result_of_calculation = integers[0]
+        for i in range(len(integers) - 1):
+            if signs[i] == '+':
+                result_of_calculation += integers[i+1]
+            elif signs[i] == '-':
+                result_of_calculation -= integers[i+1]
+        return result_of_calculation
+    except IndexError:
+        return "Invalid expression"
 
 
+commands = ['/exit', '/help']
 while True:
     inp = input()
-    if inp == '/exit':
+    if inp.startswith("/") and inp not in commands:
+        print("Unknown command")
+    elif inp == '/exit':
         print("Bye!")
         break
     elif inp == '/help':
-        print('The program excepts the expression, determines the sign in in (double minus turns to plus), and calculates the expression')
+        print('The program excepts the expression, determines the sign in it (double minus becomes plus), and calculates the expression')
     else:
         x = list(map(str, inp.split()))
         if len(x) == 0:
             continue
+        elif (len(determine_sign(x)) == 0) and (len(x) == 2):
+            print("Invalid expression")
         else:
-            array_of_signs = determine_sign(x)
-            array_of_integers = determine_ints(x)
-            final_result = calculate_expression(array_of_signs, array_of_integers)
-            print(final_result)
+            print(calculate_expression(determine_sign(x), determine_ints(x)))
